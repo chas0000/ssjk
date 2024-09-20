83338e4c73e0c7718c9bd425eed9fcc4b6a5aa16
chas0000 <86915826+chas0000@users.noreply.github.com>
2024-09-20 22:54:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
   
 # 检查文件是否存在  
-if [ ! -f /app/config/config.yaml ]; then  
+if [ ! -f ./config/config.yaml ]; then  
     echo "File does not exist, copying from backup"  
-    cp /backup/config.yaml.bak /app/config/config.yaml
+    cp ./backup/config.yaml.bak ./config/config.yaml
 fi  
   
 # 现在可以运行你的应用  
